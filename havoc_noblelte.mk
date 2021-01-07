493a780f075366201dc3dba1c5983da4222a9072
ripee <ripee.dev@gmail.com>
2021-01-07 22:39:24 +0100

    noblelte: Revise build fingerprint and Vendor security patch level

--- havoc_noblelte.mk
+++ havoc_noblelte.mk
@@ -42,6 +42,6 @@ PRODUCT_MODEL := SM-N920C
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=nobleltejv \
     TARGET_DEVICE=noblelte \
-    PRIVATE_BUILD_DESC="nobleltejv-user 7.0 NRD90M N920CXXU3CQH6 release-keys"
+    PRIVATE_BUILD_DESC="nobleltejv-user 7.0 NRD90M N920CXXS5CRH3 release-keys"
 
-BUILD_FINGERPRINT := "samsung/nobleltejv/noblelte:7.0/NRD90M/N920CXXU3CQH6:user/release-keys"
+BUILD_FINGERPRINT := "samsung/nobleltejv/noblelte:7.0/NRD90M/N920CXXS5CRH3:user/release-keys"
